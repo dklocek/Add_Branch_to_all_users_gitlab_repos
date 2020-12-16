398a64f677afd0043d3afa1326d2adbb626a0ba6
dklocek <d.klocek@wp.pl>
2020-12-16 14:45:43 +0100

    script update

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 import requests
 import sys
+
 user_id = ''
 token = ''
 branch_name = "newbranch"
@@ -8,27 +9,32 @@ cl_data = sys.argv
 
 for i in range(1, len(sys.argv)):
     if cl_data[i] == '-uid':
-        user_id = cl_data[i+1]
+        user_id = cl_data[i + 1]
     elif cl_data[i] == '-token':
-        token = cl_data[i+1]
+        token = cl_data[i + 1]
     elif cl_data[i] == '-name':
-        branch_name = cl_data[i+1]
+        branch_name = cl_data[i + 1]
     elif cl_data[i] == '-source':
-        source_branch = cl_data[i+1]
+        source_branch = cl_data[i + 1]
 
-r = requests.get('https://www.gitlab.com/api/v4/users/' + user_id + '/projects', data={'PRIVATE-TOKEN': token})
-print(r.status_code)
-if r.status_code != 200:
+response = requests.get('https://www.gitlab.com/api/v4/users/' + user_id + '/projects', data={'PRIVATE-TOKEN': token})
+print(response.status_code)
+
+if response.status_code != 200:
     exit("Error while connecting to API \n Check UserID, AccessToken oe your Internet connection")
-jr = tuple(r.json())
+
+response = tuple(response.json())
 i = 0
 ids = []
-for pr in jr:
-    ids.append(pr['id'])
+d = {'key': 'value'}
+
+for project in response:
+    ids.append(project['id'])
+    d[project['id']] = project['name']
 
 print("Trying to make branches")
 for project_id in ids:
-    print(
-        requests.post('https://www.gitlab.com/api/v4/projects/' + str(project_id) + '/repository/branches',
-                      headers={'PRIVATE-TOKEN': token},
-                      data={'branch': branch_name, 'ref': source_branch}).status_code)
+    if(requests.post('https://www.gitlab.com/api/v4/projects/' + str(project_id) + '/repository/branches',
+                     headers={'PRIVATE-TOKEN': token},
+                     data={'branch': branch_name, 'ref': source_branch}).status_code) == 201:
+        print("Created branch " + branch_name + " in project " + d[project_id])
